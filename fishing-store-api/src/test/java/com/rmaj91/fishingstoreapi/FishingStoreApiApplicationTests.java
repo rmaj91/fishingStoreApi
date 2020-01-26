a3086f9111e43c16bbff657915d821773f92ccf8
rmaj91 <rmaj91@gmail.com>
2020-01-26 10:13:58 +0100

    Initial commit

---
+++ fishing-store-api/src/test/java/com/rmaj91/fishingstoreapi/FishingStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmaj91.fishingstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
