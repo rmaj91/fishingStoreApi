c1fe3e28075e6aa8463df7b2ad99b6dbb38f4840
rmaj91 <rmaj91@gmail.com>
2020-02-22 19:24:47 +0100

    Modify security config

--- src/main/java/com/rmaj91/fishingstoreapi/security/SecurityConfig.java
+++ src/main/java/com/rmaj91/fishingstoreapi/security/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.rmaj91.fishingstoreapi.security;
 
+import com.rmaj91.fishingstoreapi.store.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -20,10 +27,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenVerifier(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/h2-console/**")
-                .permitAll();
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                .antMatchers("/test").hasRole(Role.USER.name());
     }
 
     @Override
